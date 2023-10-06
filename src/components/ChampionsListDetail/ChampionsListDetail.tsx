2cc7375b67298edebe85de09ee06cb6347cbb326
Juan <c4pjackie@gmail.com>
2023-10-06 01:40:07 -0500

    Build base layout for champions page

---
+++ src/components/ChampionsListDetail/ChampionsListDetail.tsx
@@ -0,0 +1,14 @@
+import { Champions } from "@/types/champions";
+import { FunctionComponent } from "react";
+
+type ChampionsListDetailProps = {
+  champions: Champions;
+};
+
+const ChampionsListDetail: FunctionComponent<ChampionsListDetailProps> = ({
+  champions,
+}) => {
+  return <div></div>;
+};
+
+export default ChampionsListDetail;
--- src/pages/champions/index.tsx
+++ src/pages/champions/index.tsx
@@ -1,5 +1,26 @@
-import ChampionList from "@/components/Others/ChampionList";
+import ChampionList from "@/components/ChampionList/ChampionList";
+import { Champions } from "@/types/champions";
+import { apiStaticUrl } from "@/utils/Constants/urls";
 
-export default function Champions() {
-    return <ChampionList />
+type ChampionsProps = {
+  champions: Champions;
+};
+
+export default function Champions({ champions }: ChampionsProps) {
+  return (
+    <div className="container">
+      <ChampionList {...{ champions }} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const resChampions = await fetch(apiStaticUrl.data + "/champion.json");
+  const dataChampions = await resChampions.json();
+
+  return {
+    props: {
+      champions: Object.values(dataChampions.data) as Champions,
+    },
+  };
 }

83b18a9e15164af600d52b8927e682bd1bed1494
Juan David Ramirez <juan.ramirez@perficient.com>
2023-11-09 15:51:03 -0500

    Added new metadata properties

--- src/app/summoners/[name]/page.tsx
+++ src/app/summoners/[name]/page.tsx
@@ -2,6 +2,7 @@
 
 import { Metadata } from "next";
 import SummonerProfile from "@/components/SummonerProfile/SummonerProfile";
+import { Metadata } from "next";
 import { useSearchParams } from "next/navigation";
 
 export const metadata: Metadata = {
--- src/app/tierList/page.tsx
+++ src/app/tierList/page.tsx
@@ -1,7 +1,12 @@
-"use client"
+"use client";
 
 import { Metadata } from "next";
 import TopTierList from "@/components/Others/TopTierList";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Tier List",
+};
 
 export const metadata: Metadata = {
   title: "Tier List",

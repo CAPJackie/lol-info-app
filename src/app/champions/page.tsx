2244e8220769fe3b12c4bf6c215b5f6bd7d300df
Juan <juan.ramirezm@perficient.com>
2026-03-10 22:14:16 -0500

    Optimized champions list by creating pagination

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,19 +1,8 @@
-import ChampionList from "@/components/ChampionList/ChampionList";
-import { champions } from "@/utils/Constants/champions";
 import { Metadata } from "next";
+import ClientPage from "./ClientPage";
 
 export const metadata: Metadata = {
   title: "Champions",
 };
-async function getChampions() {
-  return champions;
-}
 
-export default async function Page() {
-  const champions = await getChampions();
-  return (
-    <div className="container">
-      <ChampionList {...{ champions }} />
-    </div>
-  );
-}
+export default ClientPage;
--- src/components/ChampionsListDetail/ChampionsListDetail.tsx
+++ src/components/ChampionsListDetail/ChampionsListDetail.tsx
@@ -1,20 +1,35 @@
-import { FunctionComponent } from "react";
+"use client";
+
+import { useState } from "react";
 import ChampionCard from "../ChampionCard/ChampionCard";
 import styles from "./ChampionsListDetail.module.scss";
+
+const PAGE_SIZE = 20;
 
 type ChampionsListDetailProps = {
   champions: string[];
 };
 
-const ChampionsListDetail: FunctionComponent<ChampionsListDetailProps> = ({ champions }) => {
+const ChampionsListDetail = ({ champions }: ChampionsListDetailProps) => {
+  const [visibleCount, setVisibleCount] = useState(PAGE_SIZE);
+  const visible = champions.slice(0, visibleCount);
+  const remaining = champions.length - visibleCount;
+
   return (
-    <ul className={styles.list}>
-      {champions.map((championName) => (
-        <li key={championName} className={styles.item}>
-          <ChampionCard {...{ championName }} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className={styles.list}>
+        {visible.map((championName) => (
+          <li key={championName} className={styles.item}>
+            <ChampionCard {...{ championName }} />
+          </li>
+        ))}
+      </ul>
+      {remaining > 0 && (
+        <button className={styles.loadMore} onClick={() => setVisibleCount((c) => c + PAGE_SIZE)}>
+          Load More ({remaining} remaining)
+        </button>
+      )}
+    </>
   );
 };
 
